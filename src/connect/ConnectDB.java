9577728419f6f0496f1254b4ce0738e5bbbe064b
PhamVanThanh <phamtrung21112003@gmail.com>
2023-08-15 15:57:18 +0700

    first commit

---
+++ src/connect/ConnectDB.java
@@ -0,0 +1,43 @@
+package connect;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectDB {
+	public static Connection con = null;
+	private static ConnectDB instance = new ConnectDB();
+	public static ConnectDB getInstance() {
+		return instance;
+	}
+	public static void connect() throws SQLException {
+		String url = "jdbc:sqlserver://localhost:1433;databasename=BookStores";
+		String user = "sa";
+		String password = "sapassword";
+		con = DriverManager.getConnection(url, user, password);
+	}
+	public void disconnect() {
+		if(con!=null) {
+			try {
+				con.close();
+			}catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	public static Connection getConnection() {
+		return con;
+	}
+	
+	public static void main(String[] args) {
+		try {
+			connect();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(con);
+		
+	}
+	
+}

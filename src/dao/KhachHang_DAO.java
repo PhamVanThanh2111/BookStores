406cf03bd0600e2aa6c057cda71897f9415ad815
Tam <thanhtam0933019816@gmail.com>
2023-10-27 11:19:23 +0700

    code mới anh em lấy về dùng

--- src/dao/KhachHang_DAO.java
+++ src/dao/KhachHang_DAO.java
@@ -21,7 +21,10 @@ public class KhachHang_DAO {
 			ResultSet result = preparedStatement.executeQuery();
 
 			while (result.next()) {
-				ds.add(new KhachHang(result.getString(1), result.getString(2), result.getString(3), result.getString(4),
+				ds.add(new KhachHang(result.getString(1), 
+						result.getString(2), 
+						result.getString(3), 
+						result.getString(4),
 						result.getString(5)));
 			}
 
@@ -30,5 +33,60 @@ public class KhachHang_DAO {
 		}
 
 		return ds;
+	}
+	
+	public boolean xoaKhachHangTheoMa(String maKH) throws SQLException {
+		ConnectDB.getInstance();
+		Connection connection = ConnectDB.getConnection();
+		
+		try {
+			PreparedStatement preparedstaments = 
+					connection.prepareStatement("delete from KhachHang Where maKhachHang = '"+maKH+"'");
+				return preparedstaments.executeUpdate()>0;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		connection.close();
+		return false;
+	}
+	
+	public boolean suaKhachHangTheoMa(KhachHang khachHang) throws SQLException {
+		ConnectDB.getInstance();
+		Connection connection = ConnectDB.getConnection();
+		
+		try {
+			PreparedStatement preparedstaments = 
+					connection.prepareStatement("update KhachHang settenKhachHang=?,gioiTinh=?,soDienThoai=?,diaChi=? "
+							+ "where maKhachHang = '"+khachHang.getMaKhachHang()+"' ");
+			preparedstaments.setString(1,khachHang.getTenKhachHang());
+			preparedstaments.setString(2,khachHang.getGioiTinh());
+			preparedstaments.setString(3,khachHang.getSoDienThoai());
+			preparedstaments.setString(4,khachHang.getDiaChi());
+		} catch (SQLException e) {
+			// TODO: handle exception
+		}
+		connection.close();
+		return false;	
+	}
+	
+	public boolean themKhachHang(KhachHang khachHang) throws SQLException {
+		ConnectDB.getInstance();
+		Connection connection = ConnectDB.getConnection();
+		try {
+			PreparedStatement preparedstamets =null;
+			preparedstamets=connection.prepareStatement("insert into KhachHang values (?,?,?,?,?)");
+			preparedstamets.setString(1, khachHang.getMaKhachHang());
+			preparedstamets.setString(2, khachHang.getTenKhachHang());
+			preparedstamets.setString(3, khachHang.getGioiTinh());
+			preparedstamets.setString(4, khachHang.getSoDienThoai());
+			preparedstamets.setString(5, khachHang.getDiaChi());
+			return preparedstamets.executeUpdate() > 0;
+		} catch (SQLException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		connection.close();
+
+		return false;
 	}
 }

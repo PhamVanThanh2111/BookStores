e5711c2262f8abad2a4583abd7808f02f6f18391
PhamVanThanh <phamtrung21112003@gmail.com>
2023-11-06 16:07:17 +0700

    update chuc nang chon ngay cho ThongKe_GUI

--- src/dao/ChiTietHoaDon_DAO.java
+++ src/dao/ChiTietHoaDon_DAO.java
@@ -4,10 +4,12 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 import connect.ConnectDB;
 import entity.ChiTietHoaDon;
+import entity.HoaDon;
 
 public class ChiTietHoaDon_DAO {
 	// them nhan vien
@@ -52,4 +54,28 @@ public class ChiTietHoaDon_DAO {
 		}
 		return ds;
 	}
+	
+//	// get danh sach chi tiet hoa don theo ngay
+//		public ArrayList<ChiTietHoaDon> getListChiTietHoaDonTheoNgay(LocalDate date) { 
+//			ConnectDB.getInstance();
+//			Connection connection = ConnectDB.getConnection();
+//			ArrayList<ChiTietHoaDon> ds = new ArrayList<ChiTietHoaDon>();
+//			try {
+//				PreparedStatement preparedStatement = connection
+//						.prepareStatement("select * from ChiTietHoaDon where ngayLap = '"+ date +"'");
+//				ResultSet resultSet = preparedStatement.executeQuery();
+//				while (resultSet.next()) {
+//					HoaDon hoaDon = new HoaDon();
+//					hoaDon.setMaHoaDon(resultSet.getString(1));
+//					hoaDon.setMaNhanVien(resultSet.getString(2));
+//					hoaDon.setMaKhachHang(resultSet.getString(3));
+//					hoaDon.setNgayLap(resultSet.getDate(4));
+//					hoaDon.setThanhTien(resultSet.getFloat(5));
+//					ds.add(hoaDon);
+//				}
+//			} catch (Exception e) {
+//				// TODO: handle exception
+//			}
+//			return ds;
+//		}
 }
--- src/gui/ThongKe_GUI.java
+++ src/gui/ThongKe_GUI.java
@@ -12,12 +12,13 @@ import org.jfree.chart.renderer.category.BarRenderer;
 import org.jfree.chart.renderer.category.StandardBarPainter;
 
 import java.awt.BorderLayout;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.awt.Color;
 
+import dao.ChiTietHoaDon_DAO;
 import dao.HoaDon_DAO;
+import entity.ChiTietHoaDon;
 import entity.HoaDon;
 
 public class ThongKe_GUI extends JPanel {
@@ -26,12 +27,18 @@ public class ThongKe_GUI extends JPanel {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private DefaultCategoryDataset dataset;
-	private JFreeChart chart;
-	private CategoryPlot category;
-	private ChartPanel chartPanel;
+	private DefaultCategoryDataset datasetDoanhThu;
+	private JFreeChart chartDoanhThu;
+	private CategoryPlot categoryDoanhThu;
+	private ChartPanel chartPanelDoanhThu;
+	private DefaultCategoryDataset datasetSoLuong;
+	private JFreeChart chartSoLuong;
+	private CategoryPlot categorySoLuong;
+	private ChartPanel chartPanelSoLuong;
 	private JPanel panel;
 	private HoaDon_DAO hoaDon_DAO;
+	private JPanel panel_2;
+	private ChiTietHoaDon_DAO chiTietHoaDon_DAO;
 
 	/**
 	 * Create the panel.
@@ -39,48 +46,78 @@ public class ThongKe_GUI extends JPanel {
 	public ThongKe_GUI() {
 		// khai bao DAO
 		hoaDon_DAO = new HoaDon_DAO();
+		chiTietHoaDon_DAO = new ChiTietHoaDon_DAO();
 		
 		setLayout(null);
 		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(0, 0, 1300, 720);
+		add(panel_1);
+		panel_1.setLayout(null);
+		
 		panel = new JPanel();
-		panel.setBounds(0, 0, 1300, 720);
-		add(panel);
-		showBarChart();
+		panel.setBounds(0, 0, 891, 360);
+		panel_1.add(panel);
+		
+		panel_2 = new JPanel();
+		panel_2.setBounds(0, 360, 891, 360);
+		panel_1.add(panel_2);
+		showBarChartDoanhThu();
+		showBarChartSoLuong();
 	}
 	
-	private void showBarChart() {
-		dataset = new DefaultCategoryDataset();
+	private void showBarChartDoanhThu() {
+		datasetDoanhThu = new DefaultCategoryDataset();
 		LocalDate date = LocalDate.now();
 		DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		for (int i = 0; i <= 6; i++) {
-//			System.out.println(date.minusDays(i));
-			dataset.addValue(tinhDoanhThuTheoNgay(date.minusDays(i)), "", dateFormat.format(date.minusDays(i)).toString());
+			datasetDoanhThu.addValue(tinhDoanhThuTheoNgay(date.minusDays(i)), "Doanh thu", dateFormat.format(date.minusDays(i)).toString());
 		}
 		
-//		dataset.addValue(23, "Nam", "Nam");
-//		
-//		dataset.addValue(27, "Nữ", "Nữ");
-//		
-//		dataset.addValue(2, "Bê đe", "Bê đê");
+		chartDoanhThu = ChartFactory.createBarChart("DOANH THU", "NGÀY", "VND", datasetDoanhThu, PlotOrientation.VERTICAL, true, true, false);
 		
-		chart = ChartFactory.createBarChart("DOANH THU", "NGÀY", "VND", dataset, PlotOrientation.VERTICAL, true, true, false);
-		
-		category = chart.getCategoryPlot();
-		category.setBackgroundPaint(new Color(255, 255, 255));//change background color
+		categoryDoanhThu = chartDoanhThu.getCategoryPlot();
+		categoryDoanhThu.setBackgroundPaint(new Color(255, 255, 255));//change background color
 
 	    //set  bar chart color
+	    ((BarRenderer)categoryDoanhThu.getRenderer()).setBarPainter(new StandardBarPainter());
 
-	    ((BarRenderer)category.getRenderer()).setBarPainter(new StandardBarPainter());
-
-	    BarRenderer r = (BarRenderer)chart.getCategoryPlot().getRenderer();
-	    r.setSeriesPaint(0, new Color(129, 197, 219));
-	    r.setSeriesPaint(1, new Color(129, 197, 219));
-	    r.setSeriesPaint(2, new Color(129, 197, 219));
+	    BarRenderer rendererDoanhThu = (BarRenderer)chartDoanhThu.getCategoryPlot().getRenderer();
+	    rendererDoanhThu.setSeriesPaint(0, new Color(129, 197, 219));
 		panel.setLayout(new BorderLayout(0, 0));
 		
-		chartPanel = new ChartPanel(chart);
-		panel.add(chartPanel);
+		chartPanelDoanhThu = new ChartPanel(chartDoanhThu);
+		panel.add(chartPanelDoanhThu);
 		panel.validate();
+	}
+	
+	private void showBarChartSoLuong() {
+		datasetSoLuong = new DefaultCategoryDataset();
+		LocalDate date = LocalDate.now();
+		DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		// add value
+		for (int i = 0; i <= 6; i++) {
+			datasetSoLuong.addValue(hoaDon_DAO.getListHoaDonTheoNgay(date.minusDays(i)).size(), "Hóa đơn", dateFormat.format(date.minusDays(i)).toString());
+			datasetSoLuong.addValue(tinhSoLuongSanPhamBanDuocTheoNgay(date.minusDays(i)), "Sản phẩm", dateFormat.format(date.minusDays(i)).toString());
+		}
+		
+		chartSoLuong = ChartFactory.createBarChart("SỐ LƯỢNG SẢN PHẨM VÀ SỐ LƯỢNG HÓA ĐƠN BÁN ĐƯỢC", "NGÀY", "SỐ LƯỢNG", datasetSoLuong, PlotOrientation.VERTICAL, true, true, false);
+		
+		categorySoLuong = chartSoLuong.getCategoryPlot();
+		categorySoLuong.setBackgroundPaint(new Color(255, 255, 255));//change background color
+
+	    //set  bar chart color
+	    ((BarRenderer)categorySoLuong.getRenderer()).setBarPainter(new StandardBarPainter());
+
+	    BarRenderer rendererSoLuong = (BarRenderer)chartSoLuong.getCategoryPlot().getRenderer();
+	    rendererSoLuong.setSeriesPaint(0, new Color(129, 197, 219));
+	    rendererSoLuong.setSeriesPaint(1, new Color(56, 96, 127));
+		panel_2.setLayout(new BorderLayout(0, 0));
+		
+		chartPanelSoLuong = new ChartPanel(chartSoLuong);
+		panel_2.add(chartPanelSoLuong);
+		panel_2.validate();
 	}
 	
 	private float tinhDoanhThuTheoNgay(LocalDate date) {
@@ -89,5 +126,15 @@ public class ThongKe_GUI extends JPanel {
 			doanhThu += hoaDon.getThanhTien();
 		}
 		return doanhThu;
+	}
+	
+	private int tinhSoLuongSanPhamBanDuocTheoNgay(LocalDate date) {
+		int soLuong = 0;
+		for (HoaDon hoaDon : hoaDon_DAO.getListHoaDonTheoNgay(date)) {
+			for (ChiTietHoaDon chiTietHoaDon : chiTietHoaDon_DAO.getAllChiTietHoaDonTheoMaHoaDon(hoaDon.getMaHoaDon())) {
+				soLuong += chiTietHoaDon.getSoLuong();
+			}
+		}
+		return soLuong;
 	}
 }
